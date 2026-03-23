316b7c2d8082538116f64f7b8498792b01c870a1
Cheyenne <cheyennechoong@gmail.com>
2026-03-23 22:37:24 +0800

    SVG ID Editor

---
+++ app.py
@@ -0,0 +1,92 @@
+from flask import Flask, render_template, request, redirect, url_for
+import xml.etree.ElementTree as ET
+
+class Editor():
+    def __init__(self):
+        self.imageCode = ""
+        self.imageExport = ""
+    
+    def setImage(self, image):
+        self.imageCode = image
+        self.imageExport = image
+    
+    def getImage(self):
+        return self.imageCode
+
+    def setIDList(self, file):
+        rows = file.split("\n")
+        self.idList = [[], [], []]
+        for row in rows:
+            data = row.split(",")
+            self.idList[0].append(data[0].strip())
+            self.idList[1].append(data[1].strip())
+            self.idList[2].append(data[2].strip())
+    
+    def format(self, root):
+        for element in root.iter():
+            if element.tag.startswith("{"):
+                element.tag = element.tag.split("}", 1)[1]
+        return root
+
+    def idSVG(self):
+        root = ET.fromstring(self.imageCode)
+        for element in root.iter():
+            if "fill" in element.attrib:
+                if element.attrib["fill"] in self.idList[0]:
+                    index = self.idList[0].index(element.attrib["fill"])
+                    element.set("id", self.idList[1][index])
+                    element.set("fill", self.idList[2][index])
+        root = self.format(root)
+        self.imageCode = ET.tostring(root, encoding="unicode")
+        self.imageExport = ET.tostring(root, encoding="unicode")
+    
+    def highlight(self, id, color):
+        self.imageCode = self.imageExport
+        root = ET.fromstring(self.imageCode)
+        for element in root.iter():
+            if "id" in element.attrib:
+                if element.attrib["id"] == id:
+                    element.set("fill", color)
+        root = self.format(root)
+        self.imageCode = ET.tostring(root, encoding="unicode")
+
+editor = Editor()
+
+app = Flask(__name__)
+@app.route('/')
+def home():
+    if editor.getImage() is not None:
+        code = f"<div id='svg-preview'>{editor.getImage()}</div>"
+    else:
+        code = "<div id-'svg-preview></div>"
+    print(editor.getImage())
+    return render_template('index.html', preview = code)
+
+@app.route('/uploadSVG', methods = ['POST'])
+def uploadSVG():
+    file = request.files['svgFile']
+    content = file.read().decode('utf-8')   # decode bytes into text
+    editor.setImage(content)
+    return redirect(url_for('home'))
+
+@app.route('/uploadText', methods = ['POST'])
+def uploadText():
+    file = request.files['idFile']
+    content = file.read().decode('utf-8')
+    editor.setIDList(content)
+    return redirect(url_for('home'))
+
+@app.route('/idSVG', methods = ['POST'])
+def idSVG():
+    editor.idSVG()
+    return redirect(url_for('home'))
+
+@app.route('/highlight', methods = ['POST'])
+def highlight():
+    idInput = request.form['idInput']
+    colorInput = request.form['color']
+    editor.highlight(idInput, colorInput)
+    return redirect(url_for('home'))
+
+if __name__ == '__main__':
+    app.run(debug=True)
